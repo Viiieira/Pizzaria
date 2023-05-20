bd81f77e16d2ec07db89df55f853d412392becba
Hugo Vieira <hugo.vieira2002@hotmail.com>
2023-05-20 20:35:02 +0100

    giga update :)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,26 @@
 function openPDF() {
     window.open('./assets/docs/menu.pdf', '_blank');
 }
+
+function addClickEventToRestaurante() {
+    // Get all the restaurant elements
+    const restaurantes = document.getElementsByClassName('restaurante');
+
+    // Loop through each restaurant element
+    for (let i = 0; i < restaurantes.length; i++) {
+      // Add a click event listener to each restaurant element
+        restaurantes[i].addEventListener('click', function() {
+            const restauranteInfo = this.querySelector('.restaurante-info');
+            
+            // Change the opacity of the restaurante-info to 1
+            restauranteInfo.style.opacity = '1';
+
+            setTimeout(() => {
+                restauranteInfo.style.opacity = '0';
+            }, 5000);
+        });
+    }
+}
+  
+  // Call the function to add click event to restaurantes
+  addClickEventToRestaurante();
